32c36aa11b40ba3ee8386fca456a747450196fde
Christoph Stelz <mail@ch-st.de>
2021-06-23 21:40:19 +0200

    C++ify a bit

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 all: binomial_broadcast
 
 binomial_broadcast: binomial_broadcast.cpp
-	mpic++ binomial_broadcast.cpp -o binomial_broadcast -ggdb
+	mpic++ binomial_broadcast.cpp -o binomial_broadcast -ggdb -std=c++20 -Wall
 #binomial_broadcast.jpg: binomial_broadcast
 #	mpirun -np 7 --mca opal_warn_on_missing_libcuda 0 ./binomial_broadcast
 clean:
-	rm binomial_broadcast{,.jpg}
+	rm binomial_broadcast
--- binomial_broadcast.cpp
+++ binomial_broadcast.cpp
@@ -1,8 +1,9 @@
 #include <mpi.h>
-#include <stdio.h>
 #include <iostream>
 #include <cmath>
 #include <cassert>
+#include <cstring>
+#include <bit>
 #include <unistd.h>
 
 using namespace std;
@@ -18,54 +19,49 @@ void attach_debugger(int rank) {
     while (!attached) sleep(1);
 }
 
-int first_bit_set(int val) {
-    for (int i = 0; i < sizeof(int) * 8; i++) {
-        if (val & (1 << i))
-            return i;
-    }
-
-    return -1;
+int find_first_set(int num) {
+    return ffs(num) - 1;
 }
 
 int cancel_lsb(int val) {
     if (val == 0)
         return -1;
 
-    return val ^ (1 << first_bit_set(c_rank));
+    return val ^ (1 << find_first_set(c_rank));
 }
 
 void binomial_broadcast(int *value) {
-    int last_node = (c_rank == 0) ? ceil(log2(c_size)) : first_bit_set(c_rank);
+    int last_node = (c_rank == 0) ? ceil(log2(c_size)) : find_first_set(c_rank);
 
-
-
+    /* Receive truth from predecessor */
     if (c_rank != 0) {
         int source_rank = cancel_lsb(c_rank);
-        MPI_Recv(value, 1, MPI_INT,
-                source_rank, MPI_ANY_TAG, MPI_COMM_WORLD, 0);
+        MPI::COMM_WORLD.Recv(value, 1, MPI_INT,
+                source_rank, MPI_ANY_TAG);
     }
 
+    /* Distribute it to "child" nodes */
     for (int i = last_node - 1; i >= 0; i--) {
         int target_rank = c_rank | (1 << i);
 
         if (target_rank >= c_size) continue;
 
         cout << c_rank << " -> " << target_rank << ";" << endl;
-        MPI_Send(value, 1, MPI_INT,
-                target_rank, 0, MPI_COMM_WORLD);
+        MPI::COMM_WORLD.Send(value, 1, MPI_INT,
+                target_rank, 0);
     }
 }
 
 int main(int argc, char **argv) {
-    MPI_Init(&argc, &argv);
+    MPI::Init(argc, argv);
 
-    MPI_Comm_rank(MPI_COMM_WORLD, &c_rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &c_size);
-
+    c_rank = MPI::COMM_WORLD.Get_rank();
+    c_size = MPI::COMM_WORLD.Get_size();
 
     int val;
 
     if (c_rank == 0) {
+        // The answer to the ultimate question of life, the universe and everything
         val = 42;
     }
 
@@ -73,6 +69,6 @@ int main(int argc, char **argv) {
 
     assert(val == 42);
 
-    MPI_Finalize();
+    MPI::Finalize();
 
 }

fdc887613ac137bf64bdde282422ae9d6726b7d0
Christoph Stelz <mail@ch-st.de>
2021-10-21 15:33:29 +0200

    Add BinaryTree unit tests

---
+++ test/binary_tree_tests.cpp
@@ -0,0 +1,35 @@
+#include <gtest/gtest.h>
+#include <vector>
+#include "binary_tree.hpp"
+
+using std::vector;
+
+TEST(BinaryTreeTests, BinaryTreeStructure) {
+    const vector<uint64_t> n_summands { 3, 2, 4 };
+    DistributedBinaryTree tree(0, n_summands);
+
+    // 8 summands, we are the first rank which has 3 summands.
+    // The first three should be local then, and the others remote
+    for (int i = 0; i < 8; i++) {
+        if (i < 3) {
+            EXPECT_TRUE(tree.isLocal(i));
+        } else {
+            EXPECT_FALSE(tree.isLocal(i));
+        }
+    }
+
+    EXPECT_EQ(tree.rankFromIndex(4), 1);
+    EXPECT_EQ(tree.rankFromIndex(6), 2);
+
+    EXPECT_EQ(tree.parent(5), 4);
+    EXPECT_EQ(tree.parent(4), 0);
+    EXPECT_EQ(tree.parent(2), 0);
+
+    EXPECT_EQ(tree.rankIntersectingSummands().size(), 0);
+
+    DistributedBinaryTree treeRk1 (1, n_summands);
+    auto rankIntersectingSummands = treeRk1.rankIntersectingSummands();
+    EXPECT_EQ(rankIntersectingSummands.size(), 2);
+    EXPECT_EQ(rankIntersectingSummands[0], 3);
+    EXPECT_EQ(rankIntersectingSummands[1], 4);
+}

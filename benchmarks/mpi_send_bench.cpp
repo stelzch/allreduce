599fb8e75f7110b96bd1b59e503677b4481f2d89
Christoph Stelz <mail@ch-st.de>
2021-10-28 19:17:30 +0200

    Add benchmark for simple shmem MPI_Send

---
+++ benchmarks/mpi_send_bench.cpp
@@ -0,0 +1,41 @@
+#include <ratio>
+#include <mpi.h>
+#include <chrono>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+    int c_rank;
+    MPI_Init(&argc, &argv);
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &c_rank);
+
+
+    if (argc != 2) {
+        cout << "Usage: " << argv[0] << " <iterations>" << endl;
+        return -1;
+    }
+    const auto iterations = atoi(argv[1]);
+
+
+    auto t1 = chrono::high_resolution_clock::now();
+    double num = 2.0;
+    for (auto i = 0; i < iterations; i++) {
+        if (c_rank == 0) {
+            num += 1;
+            MPI_Send(&num, 1, MPI_DOUBLE, 1, 0, MPI_COMM_WORLD);
+        } else if(c_rank == 1) {
+            MPI_Recv(&num, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, nullptr);
+        }
+    }
+
+    auto t2 = chrono::high_resolution_clock::now();
+
+    chrono::duration<double, nano> dur = t2 - t1;
+    cout << "MPI_Send&Recv took on average " << dur.count() / iterations<< "ns" << endl;
+
+
+
+    MPI_Finalize();
+}

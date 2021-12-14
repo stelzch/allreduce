e74db46bda64c096224934e4449bb8ee20f9d9b6
Christoph Stelz <mail@ch-st.de>
2021-12-14 14:57:58 +0100

    Add benchmarking script for strong and weak scaling

--- benchmarks/benchmark.py
+++ benchmarks/benchmark.py
@@ -6,60 +6,6 @@ import re
 import platform
 import argparse
 
-parser = argparse.ArgumentParser("Run benchmark")
-parser.add_argument("-e", "--executable", default="./build/src/RADTree", type=str)
-parser.add_argument("-np", "--cluster_size", default=os.cpu_count(), type=int)
-parser.add_argument("-t", "--time_per_run", default = 30, type=float, help="Time to spend on each iteration of the benchmark")
-parser.add_argument("-f", "--flags", default="", type=str, help="Additional flags to pass to the executable under test")
-parser.add_argument("-d", "--description", default="", type=str, help="Annotation that will be put in result DB")
-parser.add_argument("--cluster-mode", help="Detect number of nodes and processors from environment", action='store_true')
-
-args = parser.parse_args()
-executable = args.executable
-datafiles = glob.glob("data/*")
-cluster_size = args.cluster_size
-modes = ["--tree", "--allreduce", "--baseline", "--reproblas"]
-expected_time_per_run = args.time_per_run # seconds for each benchmark execution
-flags = args.flags
-cluster_mode = args.cluster_mode
-
-con = sqlite3.connect('benchmarks/results.db')
-cur = con.cursor()
-cur.execute("""
-CREATE TABLE IF NOT EXISTS runs (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    date TEXT,
-    hostname TEXT,
-    revision TEXT,
-    cluster_size INTEGER,
-    description TEXT,
-    flags TEXT
-);
-""")
-cur.execute("""
-CREATE TABLE IF NOT EXISTS results (
-    run_id INTEGER,
-    datafile TEXT,
-    n_summands INTEGER,
-    repetitions INTEGER,
-    mode TEXT,
-    time_ns REAL,
-    stddev REAL,
-    output TEXT,
-    FOREIGN KEY(run_id) REFERENCES runs(id) ON DELETE CASCADE
-);
-""")
-
-git_result = subprocess.run(["git", "rev-parse", "HEAD"], capture_output=True)
-revision = str(git_result.stdout, 'utf-8') if git_result.returncode == 0 else "NONE"
-print("REVISION: ", revision)
-
-cur.execute("INSERT INTO runs(date, hostname, revision, cluster_size, description, flags) VALUES (datetime(), ?, ?, ?, ?, ?)",
-        (platform.node(), revision.strip(), cluster_size, args.description, flags))
-con.commit()
-run_id = cur.execute("SELECT MAX(ROWID) FROM runs").fetchall()[0][0]
-repetitions = 100
-
 
 def grep_number(name, string):
     regexp = f"^{name}=([+\-0-9.e]+)$"
@@ -70,45 +16,101 @@ def grep_number(name, string):
         return float(m.group(1))
 
 
-for datafile in datafiles:
-    print(datafile)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("Run benchmark")
+    parser.add_argument("-e", "--executable", default="./build/src/RADTree", type=str)
+    parser.add_argument("-np", "--cluster_size", default=os.cpu_count(), type=int)
+    parser.add_argument("-t", "--time_per_run", default = 30, type=float, help="Time to spend on each iteration of the benchmark")
+    parser.add_argument("-f", "--flags", default="", type=str, help="Additional flags to pass to the executable under test")
+    parser.add_argument("-d", "--description", default="", type=str, help="Annotation that will be put in result DB")
+    parser.add_argument("--cluster-mode", help="Detect number of nodes and processors from environment", action='store_true')
 
-    # Determine number of summands
-    n_summands = 0
-    if datafile.endswith('binpsllh'):
-        n_summands = (os.path.getsize(datafile) - 8) / 8 # -8 for 64-bit header, 8 bytes per double
-    else:
-        # text format, simply count lines
-        f = open(datafile, 'r')
-        n_summands = len(f.readlines()) - 1
-        f.close()
+    args = parser.parse_args()
+    executable = args.executable
+    datafiles = glob.glob("data/*")
+    cluster_size = args.cluster_size
+    modes = ["--tree", "--allreduce", "--baseline", "--reproblas"]
+    expected_time_per_run = args.time_per_run # seconds for each benchmark execution
+    flags = args.flags
+    cluster_mode = args.cluster_mode
 
-    last_result = None
-    for mode in modes:
-        print(f"\t\tmode = {mode[2:]}")
-        opts = ""
-        if cluster_mode:
-            opts = "--bind-to core --map-by core -report-bindings"
+    con = sqlite3.connect('benchmarks/results.db')
+    cur = con.cursor()
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS runs (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        date TEXT,
+        hostname TEXT,
+        revision TEXT,
+        cluster_size INTEGER,
+        description TEXT,
+        flags TEXT
+    );
+    """)
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS results (
+        run_id INTEGER,
+        datafile TEXT,
+        n_summands INTEGER,
+        repetitions INTEGER,
+        mode TEXT,
+        time_ns REAL,
+        stddev REAL,
+        output TEXT,
+        FOREIGN KEY(run_id) REFERENCES runs(id) ON DELETE CASCADE
+    );
+    """)
+
+    git_result = subprocess.run(["git", "rev-parse", "HEAD"], capture_output=True)
+    revision = str(git_result.stdout, 'utf-8') if git_result.returncode == 0 else "NONE"
+    print("REVISION: ", revision)
+
+    cur.execute("INSERT INTO runs(date, hostname, revision, cluster_size, description, flags) VALUES (datetime(), ?, ?, ?, ?, ?)",
+            (platform.node(), revision.strip(), cluster_size, args.description, flags))
+    con.commit()
+    run_id = cur.execute("SELECT MAX(ROWID) FROM runs").fetchall()[0][0]
+    repetitions = 100
+
+
+    for datafile in datafiles:
+        print(datafile)
+
+        # Determine number of summands
+        n_summands = 0
+        if datafile.endswith('binpsllh'):
+            n_summands = (os.path.getsize(datafile) - 8) / 8 # -8 for 64-bit header, 8 bytes per double
         else:
-            opts = f"--use-hwthread-cpus -np {cluster_size}"
-        cmd = f"mpirun {opts} {executable} -f {datafile} {mode} -r {repetitions} {flags} 2>&1"
-        print(f"\t\t\t{cmd}")
-        r = subprocess.run(cmd, shell=True, capture_output=True)
-        r.check_returncode()
-        output = r.stdout.decode("utf-8")
-        time = grep_number("avg", output)
-        stddev = grep_number("stddev", output)
-        result = grep_number("sum", output)
+            # text format, simply count lines
+            f = open(datafile, 'r')
+            n_summands = len(f.readlines()) - 1
+            f.close()
 
-        if last_result is not None:
-            deviation = abs(last_result - result)
-            if deviation > 1e-6:
-                print(f"\t\tLarge deviation to previous run detected: {deviation}")
-        last_result = result
+        last_result = None
+        for mode in modes:
+            print(f"\t\tmode = {mode[2:]}")
+            opts = ""
+            if cluster_mode:
+                opts = "--bind-to core --map-by core -report-bindings"
+            else:
+                opts = f"--use-hwthread-cpus -np {cluster_size}"
+            cmd = f"mpirun {opts} {executable} -f {datafile} {mode} -r {repetitions} {flags} 2>&1"
+            print(f"\t\t\t{cmd}")
+            r = subprocess.run(cmd, shell=True, capture_output=True)
+            r.check_returncode()
+            output = r.stdout.decode("utf-8")
+            time = grep_number("avg", output)
+            stddev = grep_number("stddev", output)
+            result = grep_number("sum", output)
 
-        cur.execute('INSERT INTO results(run_id, datafile, n_summands, repetitions, mode, time_ns, stddev, output)' \
-                'VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
-                (run_id, datafile, n_summands, repetitions, mode[2:], time, stddev, output))
-        con.commit()
+            if last_result is not None:
+                deviation = abs(last_result - result)
+                if deviation > 1e-6:
+                    print(f"\t\tLarge deviation to previous run detected: {deviation}")
+            last_result = result
 
-con.close()
+            cur.execute('INSERT INTO results(run_id, datafile, n_summands, repetitions, mode, time_ns, stddev, output)' \
+                    'VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+                    (run_id, datafile, n_summands, repetitions, mode[2:], time, stddev, output))
+            con.commit()
+
+    con.close()
---
+++ benchmarks/scaling.py
@@ -0,0 +1,90 @@
+import os
+import sqlite3
+import argparse
+import sys
+import platform
+import subprocess
+from benchmark import grep_number
+
+def get_n(datafile):
+    if datafile.endswith('binpsllh'):
+        return (os.path.getsize(datafile) - 8) / 8 # -8 for 64-bit header, 8 bytes per double
+    else:
+        # text format, simply count lines
+        with open(datafile, 'r') as f:
+            n_summands = len(f.readlines()) - 1
+
+        return n_summands
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser("Perform a benchmark to determine weak/strong scaling")
+    parser.add_argument("--executable", default="./build/src/RADTree", type=str)
+    parser.add_argument("--datafile", type=str, help="PSLLH dataset to use", required=True)
+    parser.add_argument("-n", type=int, help="Cut off datafile at n summands", default=2**64)
+    parser.add_argument("--strong", action="store_true", help="Keep overall workload constant")
+    parser.add_argument("--weak", action="store_true", help="Keep workload per rank constant")
+    parser.add_argument("--step", type=int, default=1, help="Vary processor count by this step size")
+    parser.add_argument("--min", type=int, default=1, help="Minimum number of ranks")
+    parser.add_argument("--max", type=int, default=os.cpu_count(), help="Maximum number of ranks")
+
+    args = parser.parse_args()
+    executable = args.executable
+    datafile = args.datafile
+    m_min = args.min
+    m_max = args.max
+    m_step = args.step
+    weak = args.weak
+    strong = args.strong
+    n_cutoff = args.n
+
+
+    if not os.path.exists(datafile):
+        print("Error: cannot find datafile " + datafile)
+        sys.exit(-1)
+
+    if not (strong or weak):
+        print("Error: must either supply --weak or --strong as argument")
+        sys.exit(-1)
+
+    n_datafile = get_n(datafile)
+
+    if n_cutoff > n_datafile:
+        #print("Warning: n is larger than data file")
+        pass
+
+    description = ("strong" if strong else "weak") + " scaling"
+
+    con = sqlite3.connect('benchmarks/results.db')
+    cur = con.cursor()
+    git_result = subprocess.run(["git", "rev-parse", "HEAD"], capture_output=True)
+    revision = str(git_result.stdout, 'utf-8') if git_result.returncode == 0 else "NONE"
+
+    cur.execute("INSERT INTO runs(date, hostname, revision, cluster_size, description, flags) VALUES (datetime(), ?, ?, ?, ?, ?)",
+            (platform.node(), revision.strip(), os.cpu_count(), description, ""))
+    con.commit()
+    run_id = cur.execute("SELECT MAX(ROWID) FROM runs").fetchall()[0][0]
+
+    ms = list(range(m_min, m_max + 1, m_step))
+    for m in ms:
+        n = int(min(n_datafile, n_cutoff))
+        if weak:
+            n = m * int(n / max(ms))
+        print(f"n={n}, m={m}")
+
+        opts = f"--use-hwthread-cpus -np {m}"
+        mode = "--tree"
+        repetitions = "100"
+        flags = ""
+        cmd = f"mpirun {opts} {executable} -f {datafile} {mode} -r {repetitions} {flags} 2>&1"
+        print(f"\t{cmd}")
+        r = subprocess.run(cmd, shell=True, capture_output=True)
+        r.check_returncode()
+        output = r.stdout.decode("utf-8")
+        time = grep_number("avg", output)
+        stddev = grep_number("stddev", output)
+
+        cur.execute('INSERT INTO results(run_id, datafile, n_summands, repetitions, mode, time_ns, stddev, output, ranks)' \
+                'VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
+                (run_id, datafile, n, repetitions, mode[2:], time, stddev, output, m))
+        con.commit()
+

c309ef95cc5082b894ac4c631a15f5715ff631b3
Christoph Stelz <christoph.stelz@student.kit.edu>
2022-01-14 11:51:21 +0100

    Add results, fix job script

--- misc/ompi_job.sh
+++ misc/ompi_job.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
+ml load compiler/gnu/11.1
 ml load mpi/openmpi/4.1
 ml load devel/python/3.10.0_gnu_11.1
-python3 benchmarks/benchmark.py --executable "build/src/RADTree" \
+python3 benchmarks/benchmark.py --executable="build/src/RADTree" \
 	--cluster-mode \
-	--description "bwUniCluster" \
-	--flags "--verbose" \
-	--cluster_size $SLURM_NTASKS
+	--description="bwUniCluster" \
+	--flags="--verbose" \
+	--cluster_size=$SLURM_NTASKS

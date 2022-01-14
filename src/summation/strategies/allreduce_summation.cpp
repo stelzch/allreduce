8e80cd67621c012e58133d10b9c3b41bd5952398
Christoph Stelz <stelz@i10pc136.iti.kit.edu>
2022-01-14 13:28:28 +0100

    Revert to std::accumulate

--- src/summation/strategies/allreduce_summation.cpp
+++ src/summation/strategies/allreduce_summation.cpp
@@ -8,7 +8,7 @@
 #include <mpi.h>
 
 double AllreduceSummation::accumulate() {
-    double localSum = std::reduce(summands.begin(), summands.end());
+    double localSum = std::accumulate(summands.begin(), summands.end(), 0.0);
     double globalSum = 0.0;
     MPI_Allreduce(&localSum, &globalSum, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
 

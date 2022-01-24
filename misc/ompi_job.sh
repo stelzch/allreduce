795ec9d5161d57e1170bec08444dd23fe6bd71b5
Christoph Stelz <christoph.stelz@student.kit.edu>
2022-01-24 11:31:09 +0100

    Shorten description

--- misc/ompi_job.sh
+++ misc/ompi_job.sh
@@ -4,7 +4,7 @@ ml load mpi/openmpi/4.1
 ml load devel/python/3.10.0_gnu_11.1
 python3 benchmarks/benchmark.py --executable="build/src/RADTree" \
 	--cluster-mode \
-	--description="bwUniCluster error handling" \
+	--description="bwUniCluster" \
 	--flags="--verbose" \
 	--repetitions="100" \
 	--cluster_size=$SLURM_NTASKS
